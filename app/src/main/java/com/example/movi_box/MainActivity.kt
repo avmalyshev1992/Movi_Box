c8a796db37abbf43c02eadac214cd3f6e06366db
avmalyshev <tusenko64@mail.ru>
2023-04-21 21:51:29 +0300

    Completed the first task 20.1

--- app/src/main/java/com/example/movi_box/MainActivity.kt
+++ app/src/main/java/com/example/movi_box/MainActivity.kt
@@ -5,32 +5,72 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.example.movi_box.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.topAppBar.setOnMenuItemClickListener  {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener  {
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+
     }
 
-    fun onClickToast(view: View) {
-        Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
 
-    }
 
-    fun onClickToast2(view: View) {
-        Toast.makeText(this, "Ваше избранное", Toast.LENGTH_SHORT).show()
-    }
 
-    fun onClickToast3(view: View) {
-        Toast.makeText(this, "Смотреть позже", Toast.LENGTH_SHORT).show()
-    }
+    //fun onClickToast(view: View) {
+     //   Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
 
-    fun onClickToast4(view: View) {
-        Toast.makeText(this, "Ваши подборки", Toast.LENGTH_SHORT).show()
-    }
+    //}
 
-    fun onClickToast5(view: View) {
-        Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-    }
+   // fun onClickToast2(view: View) {
+    //    Toast.makeText(this, "Ваше избранное", Toast.LENGTH_SHORT).show()
+  //  }
+
+   // fun onClickToast3(view: View) {
+   //     Toast.makeText(this, "Смотреть позже", Toast.LENGTH_SHORT).show()
+   // }
+
+   // fun onClickToast4(view: View) {
+   //     Toast.makeText(this, "Ваши подборки", Toast.LENGTH_SHORT).show()
+   // }
+
+    //fun onClickToast5(view: View) {
+    //    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+   // }
 }
