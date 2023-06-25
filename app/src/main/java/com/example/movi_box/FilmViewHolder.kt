a9ebed48eddcff3b0113a7491cc2893b6f989eb7
avmalyshev <tusenko64@mail.ru>
2023-06-25 22:28:55 +0300

    Сделатно проявление фрагмента через Circular Reveal Animation. Добавлено еще два фрагмента под «Посмотреть позже» и «Подборки», анимировать их появление.

--- app/src/main/java/com/example/movi_box/FilmViewHolder.kt
+++ app/src/main/java/com/example/movi_box/FilmViewHolder.kt
@@ -2,19 +2,33 @@ package com.example.movi_box
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.movi_box.databinding.FilmItemBinding
 
 
+//В конструктор класс передается layout, который мы создали(film_item.xml)
 class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     val filmBinding = FilmItemBinding.bind(itemView)
 
 
+
+    //В этом методе кладем данные из Film в наши View
     fun bind(film: Film) {
 
 
+        //Устанавливаем заголовок
         filmBinding.title.text = film.title
-        filmBinding.poster.setImageResource(film.poster)
+        //Устанавливаем постер
+        //Указываем контейнер, в котором будет "жить" наша картинка
+        Glide.with(itemView)
+            //Загружаем сам ресурс
+            .load(film.poster)
+            //Центруем изображение
+            .centerCrop()
+            //Указываем ImageView, куда будем загружать изображение
+            .into(filmBinding.poster)
+        //Устанавливаем описание
         filmBinding.description.text = film.description
     }
 }
--- app/src/main/java/com/example/movi_box/MainActivity.kt
+++ app/src/main/java/com/example/movi_box/MainActivity.kt
@@ -5,17 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movi_box.databinding.ActivityMainBinding
+import ru.coolhabit.firstapp.SelectionsFragment
+import ru.coolhabit.firstapp.WatchLaterFragment
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var filmsAdapter: FilmListRecyclerAdapter
+
     private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
+
+        initNavigation()
 
 
 
@@ -41,40 +47,53 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-         fun initNavigation() {
-
-
-
-
-
-            binding?.bottomNavigation?.setOnNavigationItemSelectedListener {
-                when (it.itemId) {
-                    R.id.favorites -> {
-                        supportFragmentManager
-                            .beginTransaction()
-                            .replace(R.id.fragment_placeholder, FavoritesFragment())
-                            .addToBackStack(null)
-                            .commit()
-                        true
-                    }
-                    R.id.favorites -> {
-                        Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.watch_later -> {
-                        Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.selections -> {
-                        Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    else -> false
+    private fun initNavigation() {
+       /* binding?.topAppBar?.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
                 }
+                else -> false
             }
+        }*/
 
+
+        binding?.bottomNavigation?.setOnItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.home -> {
+                    val tag = "home"
+                    val fragment = checkFragmentExistence(tag)
+                    //В первом параметре, если фрагмент не найден и метод вернул null, то с помощью
+                    //элвиса мы вызываем создание нового фрагмента
+                    changeFragment( fragment?: FirstFragment(), tag)
+                    true
+                }
+                R.id.favorites -> {
+                    val tag = "favorites"
+                    val fragment = checkFragmentExistence(tag)
+                    changeFragment( fragment?: FavoritesFragment(), tag)
+                    true
+                }
+                R.id.watch_later -> {
+                    val tag = "watch_later"
+                    val fragment = checkFragmentExistence(tag)
+                    changeFragment( fragment?: WatchLaterFragment(), tag)
+                    true
+                }
+                R.id.selections -> {
+                    val tag = "selections"
+                    val fragment = checkFragmentExistence(tag)
+                    changeFragment( fragment?: SelectionsFragment(), tag)
+                    true
+                }
+                else -> false
+            }
         }
-    override fun onBackPressed() {
+    }
+
+   override fun onBackPressed() {
         super.onBackPressed()
 
         AlertDialog.Builder(this)
@@ -92,6 +111,16 @@ class MainActivity : AppCompatActivity() {
             .show()
     }
 
+    //Ищем фрагмент по тэгу, если он есть то возвращаем его, если нет - то null
+    private fun checkFragmentExistence(tag: String): Fragment? = supportFragmentManager.findFragmentByTag(tag)
+
+    private fun changeFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_placeholder, fragment, tag)
+            .addToBackStack(null)
+            .commit()
+    }
 
 }
 
