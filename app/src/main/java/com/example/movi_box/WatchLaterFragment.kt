140ebb34193564e9e5bf71a8209f8e0bc414d1c9
avmalyshev <tusenko64@mail.ru>
2023-06-26 21:02:31 +0300

    Исправление ошибок

---
+++ app/src/main/java/com/example/movi_box/WatchLaterFragment.kt
@@ -0,0 +1,33 @@
+package ru.coolhabit.firstapp
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movi_box.databinding.FragmentWatchLaterBinding
+
+
+class WatchLaterFragment : Fragment() {
+
+    private var binding: FragmentWatchLaterBinding? = null
+    private val _binding: FragmentWatchLaterBinding get() = binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentWatchLaterBinding.inflate(inflater, container, false)
+        return _binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        AnimationHelper.performFragmentCircularRevealAnimation(binding!!.watchLaterFragmentRoot, requireActivity(), 1)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
