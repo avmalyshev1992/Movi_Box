f6a01f09bb9c0a66a4eb8f04b6092db6b9aa2a7a
avmalyshev <tusenko64@mail.ru>
2023-09-06 22:46:25 +0300

    Внедрён SharedPreference

--- app/src/main/java/com/example/movi_box/domain/Interactor.kt
+++ app/src/main/java/com/example/movi_box/domain/Interactor.kt
@@ -19,8 +19,13 @@ class Interactor(private val repo: MainRepository, private val retrofitService: 
         //Метод getDefaultCategoryFromPreferences() будет нам получать при каждом запросе нужный нам список фильмов
         retrofitService.getFilms(getDefaultCategoryFromPreferences(), API.KEY, "ru-RU", page).enqueue(object : Callback<TmdbResultsDto> {
             override fun onResponse(call: Call<TmdbResultsDto>, response: Response<TmdbResultsDto>) {
-                //При успехе мы вызываем метод передаем onSuccess и в этот коллбэк список фильмов
-                callback.onSuccess(Converter.convertApiListToDTOList(response.body()?.tmdbFilms))
+                //При успехе мы вызываем метод, передаем onSuccess и в этот коллбэк список фильмов
+                val list = Converter.convertApiListToDTOList(response.body()?.tmdbFilms)
+                //Кладем фильмы в бд
+                list.forEach {
+                    repo.putToDb(film = it)
+                }
+                callback.onSuccess(list)
             }
 
             override fun onFailure(call: Call<TmdbResultsDto>, t: Throwable) {
@@ -35,4 +40,6 @@ class Interactor(private val repo: MainRepository, private val retrofitService: 
     }
     //Метод для получения настроек
     fun getDefaultCategoryFromPreferences() = preferences.geDefaultCategory()
+
+    fun getFilmsFromDB(): List<Film> = repo.getAllFromDB()
 }
