ceb04ae8dd65422cc8c7afed691dbb53096fb346
avmalyshev <tusenko64@mail.ru>
2023-12-21 23:12:13 +0300

    Подключен Firebase, добавлен Promo

--- app/src/basic/java/com/exemple/movi_box/view/MainActivity.kt
+++ app/src/basic/java/com/exemple/movi_box/view/MainActivity.kt
@@ -5,14 +5,18 @@ import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.example.movi_box.App
 import com.example.movi_box.ConnectionChecker
 import com.example.movi_box.R
 import com.example.movi_box.databinding.ActivityMainBinding
 import com.example.movi_box.data.Entity.Film
 import com.example.movi_box.view.fragments.*
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 
 
 class MainActivity : AppCompatActivity() {
@@ -41,7 +45,50 @@ class MainActivity : AppCompatActivity() {
             addAction(Intent.ACTION_BATTERY_LOW)
         }
         registerReceiver(receiver, filters)
+
+
+    if (!App.instance.isPromoShown) {
+        //Получаем доступ к Remote Config
+        val firebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+        //Устанавливаем настройки
+        val configSettings = FirebaseRemoteConfigSettings.Builder()
+            .setMinimumFetchIntervalInSeconds(0)
+            .build()
+        firebaseRemoteConfig.setConfigSettingsAsync(configSettings)
+        //Вызываем метод, которые получит данные с сервера и вешаем слушатель
+        firebaseRemoteConfig.fetch()
+            .addOnCompleteListener {
+                //Если все получилось успешно
+                if (it.isSuccessful) {
+                    //активируем последний полученный конфиг с сервера
+                    firebaseRemoteConfig.activate()
+                    //Получаем ссылку
+                    val filmLink = firebaseRemoteConfig.getString("film_link")
+                    //Если поле не пустое
+                    if (filmLink.isNotBlank()) {
+                        //Ставим флаг что уже промо показали
+                        App.instance.isPromoShown = true
+                        //Включаем промо верстку
+                        binding.promoViewGroup.apply {
+                            //Делаем видимой
+                            visibility = View.VISIBLE
+                            //Анимируем появление
+                            animate()
+                                .setDuration(1500)
+                                .alpha(1f)
+                                .start()
+                            //Вызываем метод, который загрузит постер в ImageView
+                            setLinkForPoster(filmLink)
+                            //Кнопка, по нажатии на которую промо убереться(желательно сделать отдельную кнопку с крестиком)
+                            watchButton.setOnClickListener {
+                                visibility = View.GONE
+                            }
+                        }
+                    }
+                }
+            }
     }
+}
 
     override fun onDestroy() {
         super.onDestroy()
